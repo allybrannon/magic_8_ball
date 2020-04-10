86abdf653ad2d9e6440222cfa0bed96423eb7501
Ally Brannon <allison.brannon@gmail.com>
2020-04-10 13:02:25 -0400

    moved question and render into own component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React, { Component } from "react";
+import EightBall from "./components/EightBall";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    question: "",
-    answer: [],
-  };
-
-  async componentDidMount() {
-    const { question } = this.state;
-    const response = await fetch(
-      `https://8ball.delegator.com/magic/JSON/'${question}'`
-    );
-    const answerBall = await response.json();
-    console.log(answerBall);
-    this.setState({
-      userAnswer: answerBall.magic.answer,
-    });
-  }
   render() {
-    const { userAnswer } = this.state;
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <h1>Magic 8 ball</h1>
-            <p>(May the odds be ever in your favor)</p>
-            <input
-              type="text"
-              value={this.state.textInput}
-              placeholder="user name"
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit">Submit</button>
-          <p>Answer: {userAnswer}</p>
-        </form>
+      <div className="App">
+        <EightBall />
       </div>
     );
   }
--- src/components/EightBall.jsx
+++ src/components/EightBall.jsx
@@ -1,1 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+
+class EightBall extends Component {
+  state = {
+    question: "",
+    answer: [],
+  };
+
+  async componentDidMount() {
+    const { question } = this.state;
+    const response = await fetch(
+      `https://8ball.delegator.com/magic/JSON/'${question}'`
+    );
+    const answerBall = await response.json();
+    console.log(answerBall);
+    this.setState({
+      userAnswer: answerBall.magic.answer,
+    });
+  }
+  render() {
+    const { userAnswer } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <h1>Magic 8 ball</h1>
+            <p>(May the odds be ever in your favor)</p>
+            <input
+              type="text"
+              value={this.state.textInput}
+              placeholder="user name"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+          <p>Answer: {userAnswer}</p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EightBall;
